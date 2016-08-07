453e0571f116054d4b4fa2f70bcdb70d9e6db39b
Cortney Lane <cdlane124@gmail.com>
2016-08-07 15:58:33 -0700

    more boilerplate created; ready to begin writing some code

--- lab-cortney/app/entry.js
+++ lab-cortney/app/entry.js
@@ -5,3 +5,9 @@ require('./scss/base.scss');
 
 const angular = require('angular');
 angular.module('noteApp', []);
+
+require('./component/app-main');
+require('./component/app-note');
+require('./component/app-list');
+require('./component/app-create-note');
+require('./component/app-create-list');
