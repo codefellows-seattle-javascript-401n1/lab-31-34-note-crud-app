b6d2d09eb4b5bb0aa495c6dd64ab898271ca4565
Andrew Watson <andrewpeterwatson@gmail.com>
2016-08-14 09:25:19 -0700

    note app up and running

--- lab-andrew/webpack.config.js
+++ lab-andrew/webpack.config.js
@@ -1,23 +1,24 @@
 'use strict';
 
-// npm modules
+
 const webpack = require('webpack');
 const CleanPlugin = require('clean-webpack-plugin');
 
 const autoprefixer = require('autoprefixer');
 const ExtractText = require('extract-text-webpack-plugin');
 
-// module constants
+
 const production = process.env.NODE_ENV === 'production';
 const apiURL = process.env.API_URL || 'http://localhost:3000';
 
 
-// webpack config
+
 var plugins = [
   new ExtractText('bundle.css'),
   new webpack.DefinePlugin({
     __API_URL__: JSON.stringify(apiURL),
-  }),
+    __DEBUG__: JSON.stringify(!production)
+  })
 ];
 
 if (production){
