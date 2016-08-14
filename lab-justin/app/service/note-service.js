7a5f7582fcf217b50243cae708dd139f56c349c7
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2016-08-14 13:13:06 -0700

    completed

--- lab-justin/app/service/note-service.js
+++ lab-justin/app/service/note-service.js
@@ -46,39 +46,6 @@ function noteService($log, $q, $http){
       });
     });
   };
-  // TODO: add properties to service
-  // TODO: methods to service
-  // service.fetchNotes = function(){
-  //   $log.debug('noteService.fetchNotes');
-  //   return $q((resolve, reject) => {
-  //     $http.get(url, config)
-  //     .then( res => {
-  //       $log.log(`GET ${url}: ${res.status} noteService.fetchNotes: GET-success!`);
-  //       resolve(res.data);
-  //     })
-  //     .catch( err => {
-  //       $log.log(`GET ${url}: ${err.status} noteService.fetchNotes: GET-failure!`);
-  //       reject(err);
-  //     });
-  //   });
-  // };
-  // service.updateNote = function(data){
-  //   $log.debug('noteService.updateNote');
-  //   let res;
-  //   return $q((resolve, reject) => {
-  //     $http.put(`${url}/${data._id}`, data, config)
-  //     .then( lists => {
-  //       $log.log(`PUT ${url}: ${res.status} noteService.updateNote: PUT-success!`);
-  //       service.lists.forEach((list, index) => {
-  //         if(list._id === res.data._id) return this.lists[index] = res.data;
-  //       });
-  //       resolve(res.data);
-  //     })
-  //     .catch( err => {
-  //       $log.log(`PUT ${url}: ${err.status} noteService.updateNote: PUT-success!`);
-  //       reject(err);
-  //     });
-  //   });
-  // };
+
   return service;
 }
