ffdf331ad6263a9db6768a09f1d567007a9e23c9
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-08-14 10:01:57 -0700

    updated eslintrc and test for list service

--- lab-taylor/test/list-service-test.js
+++ lab-taylor/test/list-service-test.js
@@ -20,7 +20,42 @@ describe('testing listService', function() {
     this.httpBackend.verifyNoOutstandingExpectation();
   });
 
-  it('createNote should return a note', () => {
-    expect(false).toBe(false);
+  it('createList should return a list', () => {
+    this.httpBackend.expectPOST(baseUrl, {name: 'testList'}, headers)
+    .respond(200, {_id: '0', name: 'testList', notes:[]});
+
+    this.listService.createList({name: 'testList'})
+    .then(list => {
+      expect(list._id).toBe('0');
+      expect(list.name).toBe('testList');
+      expect(Array.isArray(list.notes)).toBe(true);
+    })
+    .catch(err => console.log(err));
+    this.httpBackend.flush();
+  });
+
+  it('fetchLists should return an array of lists', () => {
+    this.httpBackend.expectGET(baseUrl)
+    .respond(200, [{_id: '0', name: 'testList', notes: []}]);
+
+    this.listService.fetchLists()
+    .then(lists => {
+      expect(Array.isArray(lists)).toBe(true);
+      expect(lists.length).toBe(1);
+      expect(lists[0]).toBe.an('object');
+      expect(lists[0]).toEqual({_id: '0', name: 'testList', notes: []});
+    });
+  });
+
+  it('deleteList should return the deleted list', () => {
+    this.httpBackend.expectDELETE(`${baseUrl}/1234`)
+    .respond(204, {_id:'1234', name:'testList', notes:[]});
+
+    this.listService.deleteList('1234')
+    .then(list => {
+      expect(list._id).toBe('1234');
+      expect(list.name).toBe('testList');
+      expect(Array.isArray(list.notes)).toBe(true);
+    });
   });
 });
