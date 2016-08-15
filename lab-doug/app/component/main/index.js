f422496ed18424e3480440203e2bfec3f521c319
Doug Popadince <gsmatth@icloud.com>
2016-08-15 15:26:20 -0700

    modified maincontroller to add this.lists property

--- lab-doug/app/component/main/index.js
+++ lab-doug/app/component/main/index.js
@@ -15,13 +15,19 @@ angular.module('listApp').directive('appMain', function(){
   };
 });
 
+
 /**
- * MainController - populates the user page with lists
+ * MainController - populates the main.html template
  *
- * @param  {angular service} listService is a globally available service used for list CRUD
- * @return {array of lists}             updates the main template with updated lists when changes are made in the lists
+ * @param  {angular service} listService provides ability to perfrom CRUD on lists in the backend db as well as updating listService "list" array.
+ * @return {reference to "lists" array in listService} creates the "lists" property on the MainController which references the "lists" array in listService object. The main html template uses angular data binding to this MainController.lists property, to populate current lists on the page.
  */
 function MainController(listService){
-  this.lists = listService.lists;
-  listService.fetchLists();
+  listService.fetchLists()
+  .then((lists) => {
+    this.lists = lists;
+  })
+  .catch((err) => {
+    console.log('mainController ListService.fetchLists threw an error: ', err);
+  });
 }
