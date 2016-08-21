f532de787e8fabdc6d5fa003fcfcfa407ce6d84b
Cortney Lane <cdlane124@gmail.com>
2016-08-21 10:38:40 -0700

    wrote working fetchLists test, but updateList is failing...

--- lab-cortney/test/list-service-test.js
+++ lab-cortney/test/list-service-test.js
@@ -37,4 +37,56 @@ describe('testing listService', function() {
 
     this.$httpBackend.flush();
   });
+
+  it('fetchLists should fetch a list', () => {
+
+    beforeEach( () => { // create list to fetch and update
+      this.listService.createList({name: 'example list 2'})
+      .then( (list) => {
+        resolve(list);
+      })
+      .catch( (err) => {
+        reject(err);
+      });
+    });
+
+    this.$httpBackend.expectGET(baseUrl, {'Accept': 'application/json'})
+    .respond(200, {status: 'success', _id: '12345678', name: 'example list 2', notes: [], _v: 0});
+
+    this.listService.fetchLists()
+    .then( (list) => {
+      expect(list._id).toBe('12345678');
+      expect(list.name).toBe('example list 2');
+    })
+    .catch( (err) => {
+      expect(err).toBe(null);
+    });
+
+    this.$httpBackend.flush();
+  });
+
+  it('updateList should update a list', () => {
+    beforeEach( () => { // create list to fetch and update
+      this.listService.createList({name: 'example list 3'})
+      .then( (list) => {
+        resolve(list);
+      })
+      .catch( (err) => {
+        reject(err);
+      });
+    });
+
+    this.$httpBackend.expectPUT(`${baseUrl}/12345678`, {name: 'updated example list 3'}, headers)
+    .respond(200, {status: 'success', _id: '12345678', name: 'updated example list 3', notes: [], _v: 0});
+
+    this.listService.updateList(`${baseUrl}/12345678`, {name: 'updated example list 3'}, headers)
+    .then( (list) => {
+      expect(list._id).toBe('12345678');
+      expect(list.name).toBe('updated example list 3');
+    })
+    .catch( (err) => {
+      expect(err).toBe(null);
+    });
+  });
+
 }); // end listService test module
