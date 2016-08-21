84d8d6037d4096edd0f0139a86f01a19c8d5b9cb
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-08-21 11:30:11 -0700

    delete list test success

--- lab-anthony/test/list-service-test.js
+++ lab-anthony/test/list-service-test.js
@@ -9,7 +9,7 @@ var headers = {
   'Accept': 'application/json'
 };
 
-describe('Testing the List-Service', function(){
+describe('Testing the listService', function(){
   beforeEach(() => {
     angular.mock.module('noteApp');
     angular.mock.inject((listService, $httpBackend) => {
@@ -23,14 +23,13 @@ describe('Testing the List-Service', function(){
     this.$httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('Create-List should return a note', () => {
+  it('createList should return a list', () => {
     this.$httpBackend.expectPOST(baseUrl, {name: 'test list'}, headers)
     .respond(200, {_id: '12345', name: 'test list', notes: [], _v: 0});
 
     this.listService.createList({name: 'test list'})
     .then((list) => {
-      console.log('LIST IS:::', list);
-      expect(list.id).toBe('12345');
+      expect(list._id).toBe('12345');
       expect(list.name).toBe('test list');
       expect(Array.isArray(list.notes)).toBe(true);
     })
@@ -41,4 +40,38 @@ describe('Testing the List-Service', function(){
     this.$httpBackend.flush();
   });
 
-}); //CLOSE BLOCK
+  it('getLists should return a list', () => {
+    this.$httpBackend.expectGET(baseUrl)
+    .respond(200, [{test: true}]);
+
+    this.listService.getLists()
+    .then((lists) => {
+      expect(lists).toEqual([{test: true}]);
+      expect(lists[0].test).toEqual(true);
+    })
+    .catch((err) => {
+      expect(err).toBe(undefined);
+    });
+
+    this.$httpBackend.flush();
+  });
+
+  it('deleteList should remove a list', () => {
+    this.listService.lists = [{_id: '12345'}, {_id: '54321'}];
+
+    this.$httpBackend.expectDELETE(`${baseUrl}/12345`)
+    .respond(200, [{_id: '54321'}]);
+
+    this.listService.deleteList('12345')
+    .then((lists) => {
+      expect(lists).toEqual([{_id: '54321'}]);
+      expect(this.listService.lists).toEqual([{_id: '54321'}]);
+    })
+    .catch((err) => {
+      expect(err).toBe(undefined);
+    });
+
+    this.$httpBackend.flush();
+  });
+
+});
