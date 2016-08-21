1e6f6e1444c51591392ae26a978c040ad9bdc21d
muddybeard210 <mattsmith203@hotmail.com>
2016-08-21 10:41:32 -0700

    removed commented out code

--- lab-matt/test/list-controller-test.js
+++ lab-matt/test/list-controller-test.js
@@ -1,73 +1,10 @@
-// 'use strict';
-//
-// describe('testing ListController', function() {
-//   beforeEach(() => {
-//     angular.mock.module('demoApp');
-//     angular.mock.inject(($controller, $httpBackend) => {
-//       this.$httpBackend = $httpBackend;
-//       this.listCtrl = new $controller('ListController');
-//     });
-//   });
-//
-//   it('should have the controller', () => {
-//     expect(typeof this.listCtrl).toBe('object');
-//     expect(typeof this.listCtrl.createNote).toBe('function');
-//     expect(typeof this.listCtrl.deleteNote).toBe('function');
-//   });
-//
-//
-//   describe('testing listCtrl.createNote', () => {
-//     let baseUrl = 'http://localhost:3000/api';
-//
-//     let headers = {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json'
-//     };
-//
-//     beforeEach(() => {
-//       this.listCtrl.list = {name: 'Mellon', notes: [{_id: '1234567890', name: 'New Name', content: 'New Content', listId: '0987654321', _v: 0}]};
-//       this.$httpBackend.expectPOST(`${baseUrl}/note`, {name: 'Apple', content: 'Orange'}, headers)
-//       .respond(200, {name: 'Apple', content: 'Orange', _id: '0987654321', _v: 0});
-//     });
-//
-//     it('should return a note', () => {
-//       this.listCtrl.createNote({name: 'Apple', content: 'Orange'}).then(note => {
-//         console.log('Note: ', note);
-//         expect(note.name).toBe('Apple');
-//       }).catch(err => {
-//         expect(err).toBe(undefined);
-//       });
-//       this.$httpBackend.flush();
-//     });
-//
-//   });
-//   describe('testing listCtrl.deleteNote', () => {
-//     beforeEach(() => {
-//       let baseUrl = 'http://localhost:3000/api';
-//
-//       this.listCtrl.list = {name: 'New List', notes: []};
-//       this.$httpBackend.expectDELETE(`${baseUrl}/note/0987654321`).respond(200, {_id: '0987654321', name: 'New Note', content: 'New Content'});
-//     });
-//     it('should delete a note', () => {
-//       this.listCtrl.deleteNote('0987654321')
-//       .then(() => {
-//         expect(this.listCtrl.list.notes.length).toBe(0);
-//       });
-//       this.httpBackend.flush();
-//     });
-//   });
-// });
-
-
 'use strict';
 
-const controllerUrl = 'http://localhost:3000/api';
-
-describe('testing ListController', () => {
+describe('testing ListController', function() {
   beforeEach(() => {
     angular.mock.module('demoApp');
     angular.mock.inject(($controller, $httpBackend) => {
-      this.httpBackend = $httpBackend;
+      this.$httpBackend = $httpBackend;
       this.listCtrl = new $controller('ListController');
     });
   });
@@ -78,37 +15,45 @@ describe('testing ListController', () => {
     expect(typeof this.listCtrl.deleteNote).toBe('function');
   });
 
+
   describe('testing listCtrl.createNote', () => {
+    let baseUrl = 'http://localhost:3000/api';
+
+    let headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+
     beforeEach(() => {
-      this.listCtrl.list = {name: 'testList', notes: []};
-      this.httpBackend.expectPOST(`${controllerUrl}/note`, {name: 'testNote', content: 'testContent'})
-      .respond(200, {_id: '1234', name: 'testNote', content:'testContent'});
+      this.listCtrl.list = {name: 'Mellon', notes: [{_id: '1234567890', name: 'New Name', content: 'New Content', listId: '0987654321', _v: 0}]};
+      this.$httpBackend.expectPOST(`${baseUrl}/note`, {name: 'Apple', content: 'Orange'}, headers)
+      .respond(200, {name: 'Apple', content: 'Orange', _id: '0987654321', _v: 0});
     });
 
-    it('should return a list', () => {
-      this.listCtrl.createNote({name:'testNote', content:'testContent'})
-      .then(note => {
-        expect(note.name).toBe('testNote');
-        expect(note.content).toBe('testContent');
-        expect(this.listCtrl.list.notes[0].name).toBe('testNote');
+    it('should return a note', () => {
+      this.listCtrl.createNote({name: 'Apple', content: 'Orange'}).then(note => {
+        console.log('Note: ', note);
+        expect(note.name).toBe('Apple');
+      }).catch(err => {
+        expect(err).toBe(undefined);
       });
-      this.httpBackend.flush();
+      this.$httpBackend.flush();
     });
-  });
 
+  });
   describe('testing listCtrl.deleteNote', () => {
     beforeEach(() => {
-      this.listCtrl.list = {name: 'testList', notes: []};
-      this.httpBackend.expectDELETE(`${controllerUrl}/note/1234`)
-    .respond(200, {_id: '1234', name: 'testNote', content:'testContent'});
-    });
+      let baseUrl = 'http://localhost:3000/api';
 
+      this.listCtrl.list = {name: 'New List', notes: []};
+      this.$httpBackend.expectDELETE(`${baseUrl}/note/0987654321`).respond(200, {_id: '0987654321', name: 'New Note', content: 'New Content'});
+    });
     it('should delete a note', () => {
-      this.listCtrl.deleteNote('1234')
+      this.listCtrl.deleteNote('0987654321')
       .then(() => {
         expect(this.listCtrl.list.notes.length).toBe(0);
       });
-      this.httpBackend.flush();
+      this.$httpBackend.flush();
     });
   });
 });
