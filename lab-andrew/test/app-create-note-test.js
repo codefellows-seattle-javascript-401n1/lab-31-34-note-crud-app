09c0b83218032dd1a79507594e47d92189983b75
Andrew Watson <andrewpeterwatson@gmail.com>
2016-08-21 22:26:25 -0700

    directive tests passing

--- lab-andrew/test/app-create-note-test.js
+++ lab-andrew/test/app-create-note-test.js
@@ -1,30 +1,46 @@
-// 'use strict';
-//
-// describe('testing app-create-note-form', function() => {
-//   beforeEach(() => {
-//     angular.mock.module('noteApp')
-//     angular.mock.inject(($compile, $rootScope) => {
-//       let scope = $rootScope.$new();
-//
-//       scope.createNote = (function(note) {
-//         this.note = note
-//       }).bind(this)
-//
-//       let elem = angular.element('<app-create-note-form.')
-//       this.appCreateNoteForm = $compile(elem)(scope)
-//       scope.$digest()
-//     })
-//   })
-//
-//   it('should run a test', () => {
-//     let iScope = this.appCreateNoteForm.isolateScope()
-//     iScope.note = {
-//       name: 'troll',
-//       content: 'troll talk'
-//     }
-//
-//     iScope.createNote()
-//     iScope.$diget()
-//     expect('false').toBe('false')
-//   })
-// })
+'use strict';
+
+const noteBaseUrl = 'http://localhost:3000/api/note';
+const noteHeaders = {
+  'Content-Type' :'applicaiton/json',
+  'Accept':'application/json'
+};
+
+describe('testing create note service', function() {
+  beforeEach(() => {
+    angular.mock.module('noteApp');
+    angular.mock.inject((noteService, $httpBackend) => {
+      this.noteService = noteService;
+      this.httpBackend = $httpBackend;
+    });
+  });
+
+  afterEach(() => {
+    this.httpBackend.verifyNoOutstandingRequest();
+    this.httpBackend.verifyNoOutstandingExpectation();
+  });
+
+  it('should create a note', () => {
+    this.httpBackend.expectPOST(noteBaseUrl, {name: 'test troll', listId: '1234', content: 'test content'}, noteHeaders)
+    .respond(200, {_id: '1234', name: 'test troll', listId: '1234', content: 'test content'});
+
+    this.noteService.createNote({name: 'test troll', listId: '1234', content: 'test content'})
+    .then(note => {
+      expect(note.name).toBe('test troll');
+      expect(note.listId).toBe('1234');
+      expect(note.content).toBe('test content');
+    });
+  });
+
+
+  it('deleteNote should create a note', () => {
+    this.httpBackend.expectDELETE(`${noteBaseUrl}/1234`,{'Accept': 'application/json'})
+    .respond(200, {_id: '1', name: 'test troll', content: 'test content'});
+    this.noteService.deleteNote('1234')
+    .then(note => {
+      expect(note.name).toBe('test troll');
+      expect(note.listId).toBe('1234');
+      expect(note.content).toBe('test content');
+    });
+  });
+});
--- lab-andrew/test/app-list-test.js
+++ lab-andrew/test/app-list-test.js
@@ -1,27 +1,27 @@
-// 'use strict';
-//
-//
-// describe(' test for app-list', () => {
-//   beforeEach(() => {
-//     angular.mock.module('noteApp');
-//     angular.mock.inject(($compile, $rootScope) => {
-//       let scope = $rootScope.$new();
-//       scope.list = {
-//         id: '12345',
-//         name: 'troll',
-//         notes: []
-//       };
-//
-//       let elem = angular.element('app-list list="list></app-list>"');
-//       this.appList = $compile(elem)(scope);
-//       scope.$digest();
-//     });
-//   });
-//
-//   it('should return a name', () => {
-//     let isolateScope = this.appList.isolateScope();
-//     let header = this.appList.find('h2');
-//     expect(isolateScope.listCtrl.list.name).toBe('troll');
-//     expect(header.text().trim()).toContain(isolateScope.listCtrl.list.name);
-//   });
-// });
+'use strict';
+
+
+describe(' test for app-list', () => {
+  beforeEach(() => {
+    angular.mock.module('noteApp');
+    angular.mock.inject(($compile, $rootScope) => {
+      let scope = $rootScope.$new();
+      scope.list = {
+        id: '12345',
+        name: 'troll',
+        notes: []
+      };
+
+      let elem = angular.element('app-list list="list"></app-list>');
+      this.appList = $compile(elem)(scope);
+      scope.$digest();
+    });
+  });
+
+  it('should return a name', () => {
+    let isolateScope = this.appList.isolateScope();
+    let header = this.appList.find('h2');
+    expect(isolateScope.listCtrl.list.name).toBe('troll');
+    expect(header.text().trim()).toContain(isolateScope.listCtrl.list.name);
+  });
+});
